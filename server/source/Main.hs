38bf355d3b2b2424402b7d85922bfd4e2622c2ed
Jon Kristensen <jon.kristensen@lambdatrade.com>
2015-05-31 21:39:27 +0200

    Add Create endpoint for Club
    A club can be created by a POST request like so:
    
    curl -X POST -H "Content-Type: application/json" \
        --data '{ "name": "Bergsjö IF" }' localhost:3000/0.0.0/clubs

--- server/source/Main.hs
+++ server/source/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
@@ -11,15 +12,19 @@
 module Main where
 
 import Control.Applicative
+import Control.Monad.Except
 import Control.Monad.IO.Class
 import Control.Monad.Reader
 import Data.Aeson
+import Data.Default
 import Data.Typeable
 import Data.UUID
+import Data.UUID.V4
 import Database.Persist
 import Database.Persist.Quasi
 import Database.Persist.Sql
 import Database.Persist.TH
+import GHC.Generics
 import Lambdatrade
 import Network.Wai.Handler.Warp
 import Rest
@@ -33,11 +38,13 @@ import qualified Rest.Resource as R
 
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] $(persistFileWith lowerCaseSettings "schema")
 
+mkGenericJSON [t|Club|]
+
+mkJsonType ''Club def { derivedPrefix = "create", removeFields = ["uuid"] }
+
 newtype App a = App { unApi :: ReaderT ConnectionPool IO a } deriving (Applicative, Functor, Monad)
 
-instance Schema.JSONSchema Club where
-    schema _ = Schema.Object [ Schema.Field "name" True (Schema.Value (Schema.unboundedLength)) ]
-
+deriving instance Generic Club
 deriving instance Typeable Club
 
 runSql :: ReaderT ConnectionPool IO a -> App a
@@ -47,19 +54,31 @@ router :: Router App App
 router = root -/ route clubsR
   where
     clubsR :: Resource App (ReaderT Club App) Club () Void
-    clubsR = mkResourceReader { R.name = "clubs"
+    clubsR = mkResourceReader { R.create = Just createClubH
                               , R.list = const list
+                              , R.name = "clubs"
                               , R.schema = withListing () $ named [] }
-      where
-        list :: ListHandler App
-        list = mkListing jsonO $ \_ -> lift $ runSql $
-            ask >>= runSqlPool (selectList [] [Asc ClubName]) >>= return . map entityVal
+    list :: ListHandler App
+    list = mkListing jsonO $ \_ -> lift $ runSql $ do
+        pool <- ask
+        clubs <- runSqlPool (selectList [] [Asc ClubName]) pool
+        return (map entityVal clubs)
+    createClubH :: Handler App
+    createClubH = mkInputHandler (jsonI . jsonO) $ \club -> ExceptT $ do
+        r <- runSql (createClub club)
+        return $ Right r
+    createClub :: CreateClub -> ReaderT ConnectionPool IO Club
+    createClub c = do
+        uuid <- lift nextRandom
+        let club = Club uuid (createClubName c)
+        pool <- ask
+        runSqlPool (insert club) pool
+        return club
 
 api :: Api App
 api = [(mkVersion 0 0 0, Some1 router)]
 
 main :: IO ()
-main = withPool 10 $ do
-    \pool -> liftIO $ do
-        runSqlPool (runMigration migrateAll) pool
-        run 3000 (apiToApplication (\(App r) -> runReaderT r pool) api)
+main = withPool 10 $ \pool -> liftIO $ do
+    runSqlPool (runMigration migrateAll) pool
+    run 3000 (apiToApplication (\(App r) -> runReaderT r pool) api)

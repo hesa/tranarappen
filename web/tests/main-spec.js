d326c0235d019d11d5c2040f7648f9f33c5855c5
Jon Kristensen <info@jonkri.com>
2015-12-31 04:05:26 +0100

    Add a few more tests

--- web/tests/main-spec.js
+++ web/tests/main-spec.js
@@ -2,6 +2,9 @@ describe('Coachassistant', function () {
     var clubNames = ['IK Nord', 'Bergsjö IF'];
     var addedNames = [];
     var clubsLink = element(by.css('a[ui-sref="clubs"]'));
+    var teamsLink = element(by.css('a[ui-sref="teams"]'));
+    var membersLink = element(by.css('a[ui-sref="members"]'));
+    var trainingPhasesLink = element(by.css('a[ui-sref="trainingPhases"]'));
 
     browser.get('https://127.0.0.1/');
 
@@ -55,6 +58,105 @@ describe('Coachassistant', function () {
                     });
                 });
             });
+        });
+    });
+
+    it('should allow to create a team', function () {
+        teamsLink.click();
+
+        teamName = 'P10';
+
+        var modifiedName;
+
+        element.all(by.repeater('team in teams.teams')).map(function (team) {
+            return team.all(by.css('td:first-child')).first().getText();
+        }).then(function (teamTexts) {
+            var modifier = 2;
+            modifiedName = teamName;
+
+            while (true) {
+                if (teamTexts.indexOf(modifiedName) === -1) {
+                    break;
+                } else {
+                    modifiedName = teamName + '_' + modifier;
+                    modifier++;
+                }
+            }
+
+            element(by.model('teams.addTeamModel.addTeamName')).sendKeys(modifiedName);
+            element(by.css('button[type="submit"]')).click();
+        });
+
+        element.all(by.repeater('team in teams.teams')).map(function (team) {
+            return team.all(by.css('td:first-child')).first().getText();
+        }).then(function (teamTexts) {
+            expect(teamTexts.indexOf(modifiedName)).not.toEqual(-1);
+        });
+    });
+
+    it('should allow to create a member (without a team)', function () {
+        membersLink.click();
+
+        memberName = 'Jon';
+
+        var modifiedName;
+
+        element.all(by.repeater('member in members.members')).map(function (member) {
+            return member.all(by.css('td:first-child')).first().getText();
+        }).then(function (memberTexts) {
+            var modifier = 2;
+            modifiedName = memberName;
+
+            while (true) {
+                if (memberTexts.indexOf(modifiedName) === -1) {
+                    break;
+                } else {
+                    modifiedName = memberName + '_' + modifier;
+                    modifier++;
+                }
+            }
+
+            element(by.model('members.addMemberModel.addMemberName')).sendKeys(modifiedName);
+            element(by.css('button[type="submit"]')).click();
+        });
+
+        element.all(by.repeater('member in members.members')).map(function (member) {
+            return member.all(by.css('td:first-child')).first().getText();
+        }).then(function (memberTexts) {
+            expect(memberTexts.indexOf(modifiedName)).not.toEqual(-1);
+        });
+    });
+
+    it('should allow to create a training phase', function () {
+        trainingPhasesLink.click();
+
+        trainingPhaseName = 'Armhävningar';
+
+        var modifiedName;
+
+        element.all(by.repeater('trainingPhase in trainingPhases.trainingPhases')).map(function (trainingPhase) {
+            return trainingPhase.all(by.css('td:first-child')).first().getText();
+        }).then(function (trainingPhaseTexts) {
+            var modifier = 2;
+            modifiedName = trainingPhaseName;
+
+            while (true) {
+                if (trainingPhaseTexts.indexOf(modifiedName) === -1) {
+                    break;
+                } else {
+                    modifiedName = trainingPhaseName + '_' + modifier;
+                    modifier++;
+                }
+            }
+
+            element(by.model('trainingPhases.addTrainingPhaseModel.addTrainingPhaseName')).sendKeys(modifiedName);
+            element(by.css('button[type="submit"]')).click();
+        });
+
+        element.all(by.repeater('trainingPhase in trainingPhases.trainingPhases')).map(function (trainingPhase) {
+            return trainingPhase.all(by.css('td:first-child')).first().getText();
+        }).then(function (trainingPhaseTexts) {
+            expect(trainingPhaseTexts.indexOf(modifiedName)).not.toEqual(-1);
         });
     });
 });
